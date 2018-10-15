90b8945d8cc35544779a97e682424d9ca2ff3937
Brant Chen <2369047+xkdcc@users.noreply.github.com>
2018-10-14 22:51:22 -0700

    Demonstrate how to capture parameters from url by using re_path.

--- django101/urls.py
+++ django101/urls.py
@@ -14,11 +14,12 @@ Including another URLconf
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
 from django.contrib import admin
-from django.urls import path, include
+from django.urls import path, include, re_path
 from . import views
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', views.home),
     path('ajax/', include('ajax.urls')),
+    re_path(r'^time/plus/(\d{1,2})/$', views.hours_ahead)
 ]
--- django101/views.py
+++ django101/views.py
@@ -1,7 +1,21 @@
 from django.shortcuts import render
+from django.http import Http404, HttpResponse
+import datetime
+import pytz
+
 
 # Create your views here.
 
-
 def home(request):
     return render(request, 'index.html')
+
+
+def hours_ahead(request, offset):
+    try:
+        offset = int(offset)
+    except ValueError:
+        raise Http404
+
+    dt = datetime.datetime.now(tz=pytz.timezone("America/Los_Angeles")) + datetime.timedelta(hours=offset)
+    html = "<html><body>In %s hour(s), it will be  %s.</body></html>" % (offset, dt)
+    return HttpResponse(html)
