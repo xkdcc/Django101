a571b8756f0e95911a6e426b1ddd766f4cf16ea8
Brant Chen <2369047+xkdcc@users.noreply.github.com>
2018-10-16 22:40:53 -0700

    1. Replace the simple embeded HTML code of showing output of time plus one hour with better template in index.html 2. Support timezone for displaying time.

--- django101/views.py
+++ django101/views.py
@@ -1,7 +1,7 @@
 from django.shortcuts import render
 from django.http import Http404, HttpResponse
 import datetime
-import pytz
+from pytz import timezone
 
 
 # Create your views here.
@@ -16,6 +16,6 @@ def hours_ahead(request, offset):
     except ValueError:
         raise Http404
 
-    dt = datetime.datetime.now(tz=pytz.timezone("America/Los_Angeles")) + datetime.timedelta(hours=offset)
-    html = "<html><body>In %s hour(s), it will be  %s.</body></html>" % (offset, dt)
-    return HttpResponse(html)
+    fmt = "%Y-%m-%d %H:%M:%S %Z%z"
+    dt = datetime.datetime.now(timezone('US/Pacific')) + datetime.timedelta(hours=offset)
+    return render(request, 'index.html', {'offset': offset, 'dt': dt.strftime(fmt)})
