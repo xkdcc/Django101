3461c05033a9aba53a20efc6cee8be2a5e294454
Brant Chen <2369047+xkdcc@users.noreply.github.com>
2018-10-18 23:50:08 -0700

    Finish ajax demo v1, including: 1. verified that we do need the csrf js code; 2. addition works with ajax now! Fantastic!

--- ajax/views.py
+++ ajax/views.py
@@ -1,15 +1,24 @@
 from django.shortcuts import render
-
+from django.http import Http404, HttpResponse
+import json
 
 # Create your views here.
 
 
 def home(request):
-    return render(request, 'ajax/index.html')
+    return demo_ajax_v1_addition(request)
 
 
 def demo_ajax_v1_addition(request):
-    return render(request, 'ajax/index.html')
+    if request.is_ajax() and request.method == 'POST':
+        response_data = {}
+        addend1 = int(request.POST['addend1'])
+        addend2 = int(request.POST['addend2'])
+        response_data['results'] = str(addend1 + addend2)
+        data = json.dumps(response_data)
+        return HttpResponse(data, content_type='application/json')
+    else:
+        return render(request, 'ajax/index.html')
 
 
 def demo_ajax_v2_addition(request):
