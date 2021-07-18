84af8f92d68b382dabe24f819c543c738acfbbd5
Brant <xkdcc@users.noreply.github.com>
2021-07-18 18:50:16 +0800

    1. Implement v2 by using FileSystemStorage 2. Simplify v1's code 3. Add details about differences between v1 and v2

--- django101/urls.py
+++ django101/urls.py
@@ -15,6 +15,8 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path, include, re_path
+from django.conf import settings
+from django.conf.urls.static import static
 from . import views
 
 urlpatterns = [
@@ -24,3 +26,6 @@ urlpatterns = [
     path('ajax/', include('ajax.urls', namespace='ajax')),
     re_path(r'^time/plus/(\d{1,2})/$', views.hours_ahead)
 ]
+
+
+urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
--- uploadfiles/views.py
+++ uploadfiles/views.py
@@ -3,6 +3,7 @@ from django.shortcuts import render
 from django.core.files.storage import FileSystemStorage
 from django.conf import settings
 from django.contrib import messages
+from django.utils.safestring import mark_safe
 
 
 # Create your views here.
@@ -12,27 +13,41 @@ def home(request):
 
 def demo_uploadfiles_v1(request):    
     '''
+    Using open/write/close.
     Not using form, model
     '''    
-    if request.method == 'POST' and request.FILES['myfile']:
-        for key, file in request.FILES.items():
-            path = file.name
-            dest = open(os.path.join(settings.MEDIA_ROOT, file.name), 'wb') # Writing in byte as file.chunks return bytes.
-            if file.multiple_chunks:
-                for c in file.chunks():
-                    dest.write(c) # If not using wb, here it will fail as "write() argument must be str, not bytes"
-            else:
-                dest.write(file.read())
-            dest.close()
+    if request.method == 'POST' and request.FILES['v1file']:
+        myfile = request.FILES['v1file']
+        path = myfile.name
+        dest = open(os.path.join(settings.MEDIA_ROOT, myfile.name), 'wb') # Writing in byte as file.chunks return bytes.
+        if myfile.multiple_chunks:
+            for c in myfile.chunks():
+                dest.write(c) # If not using wb, here it will fail as "write() argument must be str, not bytes"
+        else:
+            dest.write(myfile.read())
+        dest.close()
             
-        messages.add_message(request, messages.SUCCESS, "Upload {} successfully!".format(file.name))
+        messages.add_message(request, messages.SUCCESS, "Upload {} successfully!".format(myfile.name))
         
         return render(request, 'uploadfiles/index.html', context = {'version': "v1"})
+    
     return render(request, 'uploadfiles/index.html')
 
 def demo_uploadfiles_v2(request):    
     '''
-    Using form but NO model
-    '''    
+    Using FileSystemStorage.
+    Not using form, model
+    '''
+    if request.method == 'POST' and request.FILES['v2file']:
+        myfile = request.FILES['v2file']
+        fs = FileSystemStorage()
+        filename = fs.save(myfile.name, myfile)
+        uploaded_file_url = fs.url(filename)
+            
+        messages.add_message(request, messages.SUCCESS, mark_safe("Upload <a href='{}'>{}</a> successfully!".format(uploaded_file_url, myfile.name)))
+        
+        return render(request, 'uploadfiles/index.html', context = {'version': "v2", 'uploaded_file_url': uploaded_file_url})
     
-    return render(request, 'uploadfiles/index.html', context = {'version': "v2"})
+        
+    
+    return render(request, 'uploadfiles/index.html')
