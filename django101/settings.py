acd24f7a62079533237ccfee714871ecc5d9516e
Brant <xkdcc@users.noreply.github.com>
2021-07-18 15:02:00 +0800

    Implemented basic file upload!

--- django101/settings.py
+++ django101/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.1/ref/settings/
 """
 
 import os
+from django.contrib.messages import constants as message_constants
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -125,3 +126,14 @@ STATICFILES_DIRS = [
     os.path.join(BASE_DIR, 'static'),
 ]
 
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+MEDIA_URL = '/media/'
+
+MESSAGE_TAGS = {
+    message_constants.DEBUG: 'alert-dark',
+    message_constants.INFO: 'alert-info',
+    message_constants.SUCCESS: 'alert-success',
+    message_constants.WARNING: 'alert-warning',
+    message_constants.ERROR: 'alert-danger',
+}
--- uploadfiles/views.py
+++ uploadfiles/views.py
@@ -1,6 +1,8 @@
+import os
 from django.shortcuts import render
-from django.http import Http404, HttpResponse, JsonResponse
-import json
+from django.core.files.storage import FileSystemStorage
+from django.conf import settings
+from django.contrib import messages
 
 
 # Create your views here.
@@ -8,5 +10,22 @@ def home(request):
     return demo_uploadfiles_v1(request)
 
 
-def demo_uploadfiles_v1(request):
+def demo_uploadfiles_v1(request):    
+    '''
+    Not using form, model
+    '''    
+    if request.method == 'POST' and request.FILES['myfile']:
+        for key, file in request.FILES.items():
+            path = file.name
+            dest = open(os.path.join(settings.MEDIA_ROOT, file.name), 'wb') # Writing in byte as file.chunks return bytes.
+            if file.multiple_chunks:
+                for c in file.chunks():
+                    dest.write(c) # If not using wb, here it will fail as "write() argument must be str, not bytes"
+            else:
+                dest.write(file.read())
+            dest.close()
+            
+        messages.add_message(request, messages.SUCCESS, "Upload {} successfully!".format(file.name))
+        
+        return render(request, 'uploadfiles/index.html', )
     return render(request, 'uploadfiles/index.html')
